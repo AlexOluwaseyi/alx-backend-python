3c72a3a4c6b58605b9800d1da435d639767476d1
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2024-05-11 08:54:49 -0700

    test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -7,12 +7,14 @@ from parameterized import parameterized
 
 
 class TestAccessNestedMap(unittest.TestCase):
+    """Test suite for Access Nested Map"""
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a", "b"), 2),
         ({"a": {"b": {"c": 3}}}, ("a", "b", "c"), 3)
     ])
     def test_access_nested_map(self, nested_map, path, result):
+        """testcase for access_nested_map"""
         self.assertEqual(access_nested_map(nested_map, path), result)
 
     @parameterized.expand([
@@ -20,16 +22,19 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": 1}, ("a", "b"))
     ])
     def test_access_nested_map_exception(self, nested_map, path):
+        """testcase for exception raised in access_nested_map"""
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
 
 
 class TestGetJson(unittest.TestCase):
+    """Class for get_json test"""
     @parameterized.expand([
         ('http://example.com', {'payload': True}),
         ('http://holberton.io', {'payload': False}),
     ])
     def test_get_json(self, url, test_payload):
+        """testcase for get_json"""
         with patch('requests.get') as mocked_get:
             mocked_get.return_value.json.return_value = test_payload
             response = get_json(url)
@@ -39,14 +44,24 @@ class TestGetJson(unittest.TestCase):
 
 
 class TestMemoize(unittest.TestCase):
-    # @patch('TestClass.a_method')
+    """
+    Class to test Memoize
+    """
     def test_memoize(self):
+        """
+        testcase for memoize
+        """
         class TestClass:
+            """
+            Test class for class TestMemoize test
+            """
             def a_method(self):
+                """defines a_method"""
                 return 42
 
             @memoize
             def a_property(self):
+                """define a_property"""
                 return self.a_method()
         with patch.object(TestClass, 'a_method') as mocked_a_method:
             TestClassInstance = TestClass()
@@ -56,4 +71,5 @@ class TestMemoize(unittest.TestCase):
 
 
 if __name__ == '__main__':
+    """Main body for test execution"""
     unittest.main()
