2f815270582ac41406ba36238a9b47495aa2efb0
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2024-05-13 22:02:28 -0700

    test_client.py

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -35,6 +35,35 @@ class TestGithubOrgClient(unittest.TestCase):
             self.assertEqual(client._public_repos_url,
                              expected_org_payload['repos_url'])
 
+    @patch('client.get_json')
+    def test_public_repos(self, mock_get_json):
+        # Setup the expected return values for the mocked functions
+        repos_payload = [
+            {'name': 'repo1', 'license': {'key': 'mit'}},
+            {'name': 'repo2', 'license': {'key': 'gpl'}}
+        ]
+        expected_repos = ['repo1', 'repo2']
+
+        # Mock return value for get_json to simulate the API response
+        mock_get_json.return_value = repos_payload
+
+        # Use patch as a context manager to mock _public_repos_url property
+        with patch('client.GithubOrgClient._public_repos_url',
+                   new_callable=PropertyMock) as mock_public_repos_url:
+            mock_public_repos_url.return_value = 'https://api.github.com\
+                                                 /orgs/google/repos'
+            client = GithubOrgClient("google")
+
+            # Call the method under test
+            result_repos = client.public_repos()
+
+            # Assert that the result matches expected repos list
+            self.assertEqual(result_repos, expected_repos)
+
+            mock_public_repos_url.assert_called_once()
+            mock_get_json.assert_called_once_with('https://api.github\
+                                                  .com/orgs/google/repos')
+
 
 if __name__ == '__main__':
     """Main body for test execution"""
